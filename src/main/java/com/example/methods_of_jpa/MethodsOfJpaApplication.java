e3769cd4f9ad552d2bf870456aff2203f4da1800
captain45 <pandadebasis036@gmail.com>
2025-05-20 20:58:22 +0530

    Add repository methods for product queries and update application logic

--- src/main/java/com/example/methods_of_jpa/MethodsOfJpaApplication.java
+++ src/main/java/com/example/methods_of_jpa/MethodsOfJpaApplication.java
@@ -1,6 +1,8 @@
 package com.example.methods_of_jpa;
 
 import java.util.List;
+import java.util.NoSuchElementException;
+import java.util.Optional;
 
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
@@ -55,11 +57,47 @@ public class MethodsOfJpaApplication implements CommandLineRunner {
 
 		// pagination
 		// Page<Product> products4 = productRepository.findAll(PageRequest.of(0, 3));
-		Page<Product> products4 = productRepository
-				.findAll(PageRequest.of(1, 3, Sort.by(Direction.DESC, "productPrice")));
+		// Page<Product> products4 = productRepository
+		// .findAll(PageRequest.of(1, 3, Sort.by(Direction.DESC, "productPrice")));
 
-		products4.forEach(System.out::println);
+		// products4.forEach(System.out::println);
 
+		// Optional<Product> optProduct = productRepository
+		// .findById("21086dfd-a21c-4c19-a29e-be25581720d4");
+		// Product existingProduct = optProduct.orElseThrow(() -> new
+		// NoSuchElementException("Product not found by id "));
+		// System.out.println("Product found by id : " + existingProduct);
+
+		// Optional<Product> optProduct =
+		// productRepository.findById("3386d76a-c7c9-4bbf-82ea-0258b9bc6e7f");
+		// Product existingProduct = optProduct.orElseThrow(() -> new
+		// NoSuchElementException("Product not found by id "));
+
+		// System.out.println("Before update : " + existingProduct);
+
+		// existingProduct.setProductName("Iphone 17 pro max");
+		// existingProduct.setProductPrice(150000.33);
+
+		// Product savedProduct = productRepository.save(existingProduct);
+		// System.out.println("Product updated : " + savedProduct);
+
+		// findByProductName
+		// Optional<Product> optProduct = productRepository.findByProductName("Iphone 17 pro max");
+		// Product productByName = optProduct.orElseThrow();
+		// System.out.println("Product found by name : " + productByName);
+
+		// findAllByProductPriceBetween
+		// List<Product> productPriceBetween = productRepository.findAllByProductPriceBetween(1000.00, 5000.00);
+		// productPriceBetween.forEach(System.out::println);
+
+		//greaterThan
+		// List<Product> productPriceGreaterThan = productRepository.findAllByProductPriceGreaterThan(10000.00, Sort.by(Direction.ASC, "productPrice"));
+		// productPriceGreaterThan.forEach(System.out::println);
+
+		// findAllByProductBrandAndProductPrice
+		Optional<Product> productByBrandAndPrice = productRepository
+				.findAllByProductBrandAndProductPrice("Apple Ind", 150000.33);
+				System.out.println("Product found by brand and price : " + productByBrandAndPrice.orElseThrow(() -> new NoSuchElementException("Product not found by brand and price")));
 	}
 
 	private List<Product> getProducts() {
--- src/main/java/com/example/methods_of_jpa/ProductRepository.java
+++ src/main/java/com/example/methods_of_jpa/ProductRepository.java
@@ -1,7 +1,19 @@
 package com.example.methods_of_jpa;
 
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ProductRepository  extends JpaRepository<Product, String> {
+    Optional<Product> findByProductName(String productName);
   
+    List<Product> findAllByProductPriceBetween(double price1, double price2);
+
+    //List<Product> findAllByProductBrand(String productBrand);
+
+    Optional<Product> findAllByProductBrandAndProductPrice(String productBrand, double productPrice);
+
+    List<Product> findAllByProductPriceGreaterThan(double price, Sort sort);
 }
